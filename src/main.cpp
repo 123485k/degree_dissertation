d25c429e9af71ff3c4684facca2bf53bae22ffa1
123485k <1758961307@qq.com>
2024-04-23 14:46:03 +0800

    增加命令行参数解析

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
-
-#include <unistd.h>
-
 #include <iostream>
+#include <string>
 
+#include "CLI/CLI.hpp"
 #include "modbus-server.hpp"
 #include "phymodule.hpp"
 #include "timer.hpp"
 using namespace mytimer;
-int main(int argc, char **argv) {
-  std::cout << "starting server\n";
-  std::string portstr = "1502";  //
-  std::cout << "listen on " << portstr << "\n";
-  ModbusSlave Server("0.0.0.0", portstr);  // init server
-  Server.run();                            // start server
+
+int main(int argc, char** argv) {
+  CLI::App app{"Modbus Server"};
+
+  std::string listen_address = "0.0.0.0";
+  int port = 502;
+
+  app.add_option("-l,--listen", listen_address, "Server listening address")
+      ->capture_default_str();
+  app.add_option("-p,--port", port, "Server listening port")
+      ->capture_default_str();
+  try {
+    app.parse(argc, argv);
+  } catch (const CLI::ParseError& e) {
+    return app.exit(e);
+  }
+
+  std::cout << "Starting server\n";
+  std::cout << "Listen on " << listen_address << ":" << port << "\n";
+
+  ModbusSlave Server(listen_address, std::to_string(port));  // init server
+  Server.run();                                              // start server
+
   SecondOderModule M;
   PeriodicTimer timer(std::chrono::milliseconds{10}, [&]() {
     M.in = Server.getRegFloat_cdab(73);
@@ -20,6 +36,7 @@ int main(int argc, char **argv) {
     Server.setInputRegFloat_dcba(73, M.out);
   });
   timer.start();
+
   while (1) {
   }
 }
